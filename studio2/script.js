1b5130ecbd866d5812188e331f2dd7433ee021e1
phamkelly9 <67294673+phamkelly9@users.noreply.github.com>
2023-05-02 13:33:21 -0700

    checkurself w/o flip

--- studio2/script.js
+++ studio2/script.js
@@ -1,26 +1,24 @@
-// let globalData;
+// from glenda: add the IIFE and use strict directive here
 
 async function getData(){
     const myMoods = await fetch('data.json');
-    const data = await data.json();
-    console.log(data);
-    // globalData = data;
-    // document.querySelector('.two').innerHTML = outputHTML1(data);
+    const data = await myMoods.json();
     updateInterface(data);
 }
 getData();
 
-// function outputHTML1(data) {
-//     let html = '<p>';
-//     html += `date: ${data.one.date}\ntime: ${data.one.time}\nplace: ${data.one.place}\ndish/drink: ${data.one.dishes/drinks}\nrating: 
-//     ${data.one.rating}`;
-//     html += '</p>';
-//     return html;
-// }
-
-getData();
-
 function updateInterface(jsonData) {
-    const keys=Object.keys(jsonData);
-    console.log(keys);
+    const keys = Object.keys(jsonData);
+    // console.log(keys);
+
+    // from glenda: here is the basic script. You can keep writing individual statements like this or put them into a forEach loop to apply the code all at once. We can talk more on Sunday about it :)
+
+    keys.forEach(function(key){
+        console.log(`.${key} p`);
+        document.querySelector(`.${key} p`).innerHTML = 
+        `<p>Date: ${jsonData[key].date}</p>
+        <p>Place: ${jsonData[key].place}</p>
+        <p>Dish/drink: ${jsonData[key].dish}</p>
+        <p>Rating: ${jsonData[key].rating}</p>`;
+    })
 }
