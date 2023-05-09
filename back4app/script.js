7e68394c68ef685f7737746f0bd1ff61c95a3c46
phamkelly9 <67294673+phamkelly9@users.noreply.github.com>
2023-05-08 20:03:45 -0700

    back4app

---
+++ back4app/script.js
@@ -0,0 +1,74 @@
+// JS here
+<script type="text/javascript" src="https://npmcdn.com/parse/dist/parse.min.js"></script>
+
+Parse.initialize("zoc0mRLsd5hjbr7qdemSjcQ3HtC3C95043ULHvKO","BrooYXjdKKX5U4lpVQ5oGcyPl3qyE33HgST8fgdJ"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+    Parse.serverURL = 'https://parseapi.back4app.com/'
+
+const newBtn = document.getElementById("newbtn");
+const editBtns = document.querySelectorAll(".fa-edit");
+const addFriendForm = document.getElementById("add-friend");
+const editFriendForm = document.getElementById("edit-friend");
+const friendList = document.querySelector("main ol");
+
+newwBtn.addEventListener("click", function(event){
+    event.preventDefault();
+    addFriendForm.className = "add-friend-onscreen";
+})
+
+addFriendForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    addFriendForm.className = "add-friend-offscreen";
+})
+
+for(let i=0; i<editBtns.length; i++){
+    editBtns[i].addEventListener("click", function(event){
+        event.preventDefault();
+        editFriendForm.className = "edit-friend-onscreen";
+    })
+}
+
+editFriendForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    editFriendForm.className = "edit-friend-offscreen";
+})
+
+async function displayFriends() {
+    const friends = Parse.Object.extend('Friends');
+    const query = new Parse.Query(friends);
+
+    const results = await query.ascending('lname').find();
+    //console.log(results);
+
+    try {
+        const results = await query.ascending('lname').find();
+
+        results.forEach(function(eachFriend) {
+            const id = eachFriend.id;
+            const lname = eachFriend.get('lname');
+            const fname = eachFriend.get('fname');
+            const email = eachFriend.get('email');
+            const facebook = eachFriend.get('facebook');
+            const twitter = eachFriend.get('twitter');
+            const instagram = eachFriend.get('instagram');
+            const linkedin = eachFriend.get('linkedin');
+    
+            const theListItem = document.createElement("li");
+            theListItem.setAttribute("id", `r-%{id}`);
+            theListItem.innerHTML = `<div class="name>${fname} ${lname} </div>
+            <div class="email"> <i class ="fas fa-envelope-square"> </i> ${email}</div>
+            <div class="social"> <a href="${facebook}"> <i class="fab fa-facebook-square"></i></a>
+            <a href="${twitter}"> <i class="fab fa-twitter-square"></i></a>
+            <a href="${instagram}"> <i class="fab fa-instagram-square"></i></a>
+            <a href="${linkedin}"> <i class="fab fa-linkedin-square"></i></a></div>
+            <i class="fas fa-edit" id="e-${id}"></i>
+            <i class="fas fa-times-circle" id="d-${id}"></i> `;
+    
+            friendList.append(theListItem);
+        });
+    } catch (error) {
+        console.error('Error while fetching Friends', error);
+    }
+};
+
+
+displayFriends();
