b4ba9433525f6f8c27f1ab772f830df5695e18a6
phamkelly9 <67294673+phamkelly9@users.noreply.github.com>
2023-06-01 13:41:22 -0700

    usability

--- capstone/ver2/script.js
+++ capstone/ver2/script.js
@@ -1,17 +1,18 @@
-Parse.initialize("5QcrVqeODVexkF0PnbxIofIs6VvdClJBHGDR54LB","xtLarTXGrYtIlcNit1vLHzgrwC5I11QE9DnuA3CU"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
-Parse.serverURL = 'https://parseapi.back4app.com/'
+Parse.initialize("5QcrVqeODVexkF0PnbxIofIs6VvdClJBHGDR54LB","xtLarTXGrYtIlcNit1vLHzgrwC5I11QE9DnuA3CU"); 
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
-AOS.init();
+// AOS.init();
 
 (function(){
     'use strict';
-
+    console.log('reading js')
     const newBtn = document.getElementById("newbtn");
 const addPost = document.getElementById("add-post");
 const postList = document.querySelector("main ol");
 
 newBtn.addEventListener("click", function(event){
     event.preventDefault();
+    console.log('clicked new button')
     document.getElementById('name').className = 'onscreen';
 })
 
@@ -20,44 +21,44 @@ addPost.addEventListener("submit", function(event){
     addPost.className = "offscreen";
 })
 
-async function displayPosts() {
-    const posts = Parse.Object.extend('Posts');
-    const query = new Parse.Query(posts);
+// async function displayPosts() {
+//     const posts = Parse.Object.extend('Posts');
+//     const query = new Parse.Query(posts);
 
-    // const results = await query.ascending('lname').find();
-    // //console.log(results);
+//     // const results = await query.ascending('lname').find();
+//     // //console.log(results);
 
-    try {
-        // const results = await query.ascending('lname').find();
+//     try {
+//         // const results = await query.ascending('lname').find();
 
-        results.forEach(function(eachPost) {
-            const id = eachPost.id;
-            const lname = eachPost.get('text');
-            // const fname = eachPost.get('fname');
-            // const email = eachPost.get('email');
-            // const facebook = eachPost.get('facebook');
-            // const twitter = eachPost.get('twitter');
-            // const instagram = eachPost.get('instagram');
-            // const linkedin = eachPost.get('linkedin');
+//         results.forEach(function(eachPost) {
+//             const id = eachPost.id;
+//             const lname = eachPost.get('text');
+//             // const fname = eachPost.get('fname');
+//             // const email = eachPost.get('email');
+//             // const facebook = eachPost.get('facebook');
+//             // const twitter = eachPost.get('twitter');
+//             // const instagram = eachPost.get('instagram');
+//             // const linkedin = eachPost.get('linkedin');
     
-            const theListItem = document.createElement("li");
-            theListItem.setAttribute("id", `r-%{id}`);
-            theListItem.innerHTML = `<div class="post">${text}</div>
+//             const theListItem = document.createElement("li");
+//             theListItem.setAttribute("id", `r-%{id}`);
+//             theListItem.innerHTML = `<div class="post">${text}</div>
 
-            // <div class="social"> <a href="${facebook}"> <i class="fab fa-facebook-square"></i></a>
-            // <a href="${twitter}"> <i class="fab fa-twitter-square"></i></a>
-            // <a href="${instagram}"> <i class="fab fa-instagram-square"></i></a>
-            // <a href="${linkedin}"> <i class="fab fa-linkedin-square"></i></a></div>
-            // <i class="fas fa-edit" id="e-${id}"></i>
-            // <i class="fas fa-times-circle" id="d-${id}"></i> `;
+//             // <div class="social"> <a href="${facebook}"> <i class="fab fa-facebook-square"></i></a>
+//             // <a href="${twitter}"> <i class="fab fa-twitter-square"></i></a>
+//             // <a href="${instagram}"> <i class="fab fa-instagram-square"></i></a>
+//             // <a href="${linkedin}"> <i class="fab fa-linkedin-square"></i></a></div>
+//             // <i class="fas fa-edit" id="e-${id}"></i>
+//             // <i class="fas fa-times-circle" id="d-${id}"></i> `;
     
-            postList.append(theListItem);
-        });
-    } catch (error) {
-        console.error('Error while fetching posts', error);
-    }
-};
+//             postList.append(theListItem);
+//         });
+//     } catch (error) {
+//         console.error('Error while fetching posts', error);
+//     }
+// };
 
 
-displayPosts();
-})
+// displayPosts();
+})()
